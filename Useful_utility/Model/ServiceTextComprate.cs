4cf5b60f266c6c22f8622505be95ab86ae286b12
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-04-24 10:03:36 +0400

    Добавьте файлы проекта.

---
+++ Useful_utility/Model/ServiceTextComprate.cs
@@ -0,0 +1,56 @@
+﻿namespace Useful_utility.Model
+{
+    public class TextComprate
+    {
+        public string ValueText { get; set; }
+        public string IntupText { get; set; }
+
+
+        public bool? IsGoog
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(IntupText))
+                    return null;
+
+                if (ValueText == IntupText)
+                    return true;
+
+                if (string.IsNullOrEmpty(ValueText) && IntupText.ToLower() == "space")
+                    return true;
+
+                if (string.IsNullOrEmpty(ValueText) && IntupText.ToLower() == "-")
+                    return true;
+
+
+                if (string.IsNullOrEmpty(ValueText) && IntupText.ToLower() == " ")
+                    return true;
+
+
+                return false;
+            }
+        }
+    }
+
+    public class ServiceTextComprate
+    {
+        public List<TextComprate> Compresses { get; set; } = new List<TextComprate>();
+
+
+        public ServiceTextComprate()
+        {
+            Compresses = new List<TextComprate>();
+        }
+
+        public ServiceTextComprate(string content)
+        {
+            var r = content.Split('\n');
+
+            foreach (var r2 in r)
+            {
+                Compresses.Add(new TextComprate { ValueText = r2.Trim() });
+            }
+        }
+
+    }
+}
