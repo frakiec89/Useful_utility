c2202310abc5a8b79a1753ade7fb7bc9d4a287ca
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-04-24 14:42:47 +0400

    ds

--- Useful_utility/Model/ServiceTextComprate.cs
+++ Useful_utility/Model/ServiceTextComprate.cs
@@ -13,8 +13,7 @@
                 if (string.IsNullOrEmpty(IntupText))
                     return null;
 
-                if (ValueText == IntupText)
-                    return true;
+
 
                 if (string.IsNullOrEmpty(ValueText) && IntupText.ToLower() == "space")
                     return true;
@@ -25,10 +24,59 @@
 
                 if (string.IsNullOrEmpty(ValueText) && IntupText.ToLower() == " ")
                     return true;
-
+                return CompreteForSpace(ValueText.Trim() , IntupText.Trim());
 
                 return false;
             }
+        }
+
+        private bool? CompreteForSpace(string valueText, string intupText)
+        {
+
+            int st1 = valueText.Select(x=>x == ' ').Count();
+            int st2 = intupText.Select(x => x == ' ').Count();
+
+            if(st1>st2)
+                return false;
+
+
+
+            List<string> strings1 = new List<string>();
+
+            foreach (var item in valueText.Split(' '))
+            {
+                if(String.IsNullOrWhiteSpace(item) == false)
+                    strings1.Add(item);
+            }
+
+
+
+            List<string> strings2 = new List<string>();
+
+            foreach (var item in intupText.Split(' '))
+            {
+                if (String.IsNullOrWhiteSpace(item) == false)
+                    strings2.Add(item);
+            }
+
+               if(strings1.Count != strings2.Count)
+                    return false;
+
+            for (int i = 0; i < strings1.Count; i++)
+            {
+                if (string.Compare(strings1[i] , strings2[i] )!= 0)
+                    return false;
+            }
+
+
+            string s  = valueText.Replace(" ", "");
+            string s1 = intupText.Replace(" ", "");
+
+            if(string.Compare(s , s1) == 0)
+                return true;
+            return false;
+
+
         }
     }
 
